6e83bafe07512aad7b70c467181f6d9e47a1934c
bottedapp <102890553+bottedapp@users.noreply.github.com>
2022-04-03 02:52:13 -0400

    Update botted.py

--- botted.py
+++ botted.py
@@ -12,27 +12,3 @@ Reddit = praw.Reddit(
 username = sys.argv[1]
 for item in Reddit.redditor(username).comments.new(limit=10):
     print(item.body)
-"""
-# Credit https://pastebin.com/wiAByySP
-Similarities = {}
-for Index, Comment in enumerate(Reddit.redditor(username).comments.new(limit=10)):
-    for CheckIndex, CheckComment in enumerate(Reddit.redditor(username).comments.new(limit=10)):
-        if not Comment.subreddit.display_name == CheckComment.subreddit.display_name:
-            continue
-        if Index != CheckIndex:
-            if Comment.subreddit.display_name in Similarities:
-                List = Similarities[Comment.subreddit.display_name]
-                List.append(difflib.SequenceMatcher(None, Comment.body, CheckComment.body).ratio())
-                Similarities[Comment.subreddit.display_name] = List
-                continue
-            Similarities[Comment.subreddit.display_name] = [difflib.SequenceMatcher(None, Comment.body, CheckComment.body).ratio()]
-SimilaritiesValues = list(Similarities.values())
-TotalSimilarities = []
-for Index, Item in enumerate(Similarities):
-    TotalSimilarities.append(sum(SimilaritiesValues[Index]) / len(SimilaritiesValues[Index]))
-try:
-    print(sum(TotalSimilarities) / len(TotalSimilarities))
-except ZeroDivisionError:
-    print(0.0)
-"""
-    print(username)
