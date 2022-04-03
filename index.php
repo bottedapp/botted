4e9d24e7e52fde071890cec59f0e70354c231c30
bottedapp <102890553+bottedapp@users.noreply.github.com>
2022-04-03 03:43:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,13 @@ if(isset($_POST['submit'])){ //check if form was submitted
 	<title>botted</title>
 	<meta name="description" content="botted app">
 	<meta name="author" content="">
-	<link rel="icon" href="/images/favicon.ico">
-	<link rel="icon" href="/images/favicon.svg" type="image/svg+xml">
-	<link rel="apple-touch-icon" href="/images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/images/favicon/site.webmanifest">
+	<link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
 	<link rel="stylesheet" href="/css/stylesheet.css">
 </head>
 <body>
