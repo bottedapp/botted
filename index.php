c40a47309beb19fbf36e1bcfb4b29c020e6bfccc
bottedapp <102890553+bottedapp@users.noreply.github.com>
2022-04-03 01:14:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-botted
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>botted</title>
+	<meta name="description" content="botted app">
+	<meta name="author" content="">
+	<link rel="icon" href="/favicon.ico">
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	<link rel="stylesheet" href="css/stylesheet.css?v=1.0">
+</head>
+<body>
+	<div class="top"><img src="/images/botted.png" width="200px"></div>
+	<div class="content"><img src="/images/bot.png" width="200px" align="center"></div>
+	<form action="/action_page.php" id="form">
+		<input type="text" id="uname" name="uname" placeholder="Username"><br><br>
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
