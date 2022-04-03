afaa6c490f3434ec05467e288933d524ff8e2fb2
bottedapp <102890553+bottedapp@users.noreply.github.com>
2022-04-03 01:44:24 -0400

    Update botted.py

--- botted.py
+++ botted.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
 import praw
 import difflib
+import sys
+
 
 Reddit = praw.Reddit(
     client_id="GgPNctP2KQdth-iX6aMGUQ",
     client_secret="6zov1gDWJ8Ij60yH3L7q6N_LnPUZHA",
     user_agent="botted 0.0.1",
 )
-username = input("Enter username:")
+username = str(sys.argv)
 for item in Reddit.redditor(username).comments.new(limit=10):
     print(item.body)
 
