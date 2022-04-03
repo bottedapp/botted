815553ad410e94db7783978047ddfa68404ebfd1
bottedapp <102890553+bottedapp@users.noreply.github.com>
2022-04-03 13:03:49 -0400

    Update botted.py

--- botted.py
+++ botted.py
@@ -15,3 +15,25 @@ i = 1
 for item in Reddit.redditor(username).comments.new(limit=10):
     print(f"comment {i}:", item.body, "<br>")
     i += 1
+
+# Credit https://pastebin.com/wiAByySP
+Similarities = {}
+for Index, Comment in enumerate(Reddit.redditor(username).comments.new(limit=10)):
+    for CheckIndex, CheckComment in enumerate(Reddit.redditor(username).comments.new(limit=10)):
+        if not Comment.subreddit.display_name == CheckComment.subreddit.display_name:
+            continue
+        if Index != CheckIndex:
+            if Comment.subreddit.display_name in Similarities:
+                List = Similarities[Comment.subreddit.display_name]
+                List.append(difflib.SequenceMatcher(None, Comment.body, CheckComment.body).ratio())
+                Similarities[Comment.subreddit.display_name] = List
+                continue
+            Similarities[Comment.subreddit.display_name] = [difflib.SequenceMatcher(None, Comment.body, CheckComment.body).ratio()]
+SimilaritiesValues = list(Similarities.values())
+TotalSimilarities = []
+for Index, Item in enumerate(Similarities):
+    TotalSimilarities.append(sum(SimilaritiesValues[Index]) / len(SimilaritiesValues[Index]))
+try:
+    print(sum(TotalSimilarities) / len(TotalSimilarities))
+except ZeroDivisionError:
+    print(0.0)
