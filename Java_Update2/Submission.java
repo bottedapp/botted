30f187639ede3cc107e5e080d7e50f423cf6e9af
leemoriarity <73798970+leemoriarity@users.noreply.github.com>
2022-04-11 12:01:04 -0400

    Update Submission.java

--- Java_Update2/Submission.java
+++ Java_Update2/Submission.java
@@ -1,6 +1,8 @@
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.IOException;
 import java.util.*;
 
@@ -12,7 +14,12 @@ public class Submission extends User {
     private int submissionSubredditCount, freeKarma = 0;
     private ArrayList commentSubreddits = new ArrayList();
 
-    public Submission(String subreddit, String name, String id, String user, Boolean verified, Boolean has_verified_email, Boolean is_gold, Boolean is_mod, Boolean is_employee, int awardee_karma, int awarder_karma, int link_karma, int comment_karma, int total_karma, Date created, String comment, boolean upvote, boolean downvote, double submissionTotalScore, String popularSubmissionSubreddit, int submissionSubredditCount, int freeKarma, ArrayList commentSubreddits) {
+    public Submission(String user) throws IOException, InterruptedException {
+        super(user);
+        analyzeSubmission();
+    }
+
+    public Submission(String subreddit, String name, String id, String user, Boolean verified, Boolean has_verified_email, Boolean is_gold, Boolean is_mod, Boolean is_employee, int awardee_karma, int awarder_karma, int link_karma, int comment_karma, int total_karma, Date created, String comment, boolean upvote, boolean downvote, double submissionTotalScore, String popularSubmissionSubreddit, int submissionSubredditCount, int freeKarma, ArrayList commentSubreddits) throws IOException {
         super(subreddit, name, id, user, verified, has_verified_email, is_gold, is_mod, is_employee, awardee_karma, awarder_karma, link_karma, comment_karma, total_karma, created, comment, upvote, downvote);
         this.submissionTotalScore = submissionTotalScore;
         this.popularSubmissionSubreddit = popularSubmissionSubreddit;
@@ -83,71 +90,7 @@ public class Submission extends User {
         this.result = result;
     }
 
-    public String readInput(String user, String input) throws IOException, InterruptedException {
-        if (!input.contains("/")) {
-            user = input;
-        }
-        if (input.startsWith("t2_")) {
-            //fullname
-        }
-        if (input.startsWith("u/") || input.contains("/u/") || input.contains("/user/")) {
-            String[] e = input.split("u/");
-            String[] f = e[1].split("/");
-            user = f[0];
-        }
-
-        if (input.contains("/comments/") && !input.contains("/comment/")) {
-            String[] c = input.split(".com/");
-            String[] d = c[1].split("/");
-            String endpoint;
-            if (d.length <= 5)
-                result = "submission";
-            endpoint = "/r/" + d[1] + "/comments/" + d[3];
-            JsonArray submissionInfo = useEndpointSubmission(endpoint);
-            JsonObject array0 = submissionInfo.get(0).getAsJsonObject();
-            JsonObject submissionData = (JsonObject) array0.getAsJsonObject().get("data");
-            JsonArray submissionChildren = submissionData.getAsJsonArray("children");
-            for (JsonElement items : submissionChildren) {
-                JsonObject data3 = (JsonObject) items.getAsJsonObject().get("data");
-                String author = String.valueOf(data3.getAsJsonObject().get("author"));
-                user = author.replace("\"", "");
-            }
-
-            if (d.length >= 6) {
-                result = "comment";
-                endpoint = "/r/" + d[1] + "/api/info?id=t1_" + d[5];
-                JsonObject info = useEndpoint(endpoint);
-                JsonObject data = info.getAsJsonObject("data");
-                JsonArray children = data.getAsJsonArray("children");
-                for (JsonElement item : children) {
-                    JsonObject data11 = (JsonObject) item.getAsJsonObject().get("data");
-                    String author = String.valueOf(data11.getAsJsonObject().get("author"));
-                    user = author.replace("\"", "");
-                }
-            }
-        }
-
-        if (input.contains("/comments/") && input.contains("/comment/")) {
-            result = "comment";
-            String[] a = input.split("/r/");
-            String[] subreddit = a[1].split("/");
-            String[] b = input.split("/comment/");
-            String[] commentId = b[1].split("/");
-            String endpoint = "/r/" + subreddit[0] + "/api/info?id=t1_" + commentId[0];
-            JsonObject info = useEndpoint(endpoint);
-            JsonObject data = info.getAsJsonObject("data");
-            JsonArray children = data.getAsJsonArray("children");
-            for (JsonElement item : children) {
-                JsonObject data1 = (JsonObject) item.getAsJsonObject().get("data");
-                String author = String.valueOf(data1.getAsJsonObject().get("author"));
-                user = author.replace("\"", "");
-            }
-        }
-        this.user = user;
-        return result;
-    }
-
-    public void analyzeSubmission() {
+    public void analyzeSubmission() throws IOException, InterruptedException {
         JsonObject submitted = useEndpoint("/user/" + user + "/submitted");
         JsonObject data = (JsonObject) submitted.get("data");
         JsonArray children = (JsonArray) data.get("children");
@@ -191,13 +134,6 @@ public class Submission extends User {
             }
         }
         submissionTotalScore = postScore / postScoreCount;
-    }
-
-    public static double findSimilarity(String x, String y) {
-        double maxLength = Double.max(x.length(), y.length());
-        if (maxLength > 0)
-            return (maxLength - StringUtils.getLevenshteinDistance(x, y)) / maxLength;
-        return 0.0;
     }
 
     @Override
