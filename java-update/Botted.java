4b441b7dd0ae99b85ac6dbcb03972b64c81c0757
leemoriarity <73798970+leemoriarity@users.noreply.github.com>
2022-04-10 20:07:41 -0400

    Update Botted.java

--- java-update/Botted.java
+++ java-update/Botted.java
@@ -1,11 +1,16 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Botted {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter username/post/comment: ");
+        String in = scan.nextLine();
         Reddit reddit = new Reddit();
-        Reddit user = new User();
-        Reddit comments = new Comments();
-        Reddit submissions = new Submissions();
+        Reddit input = new Input(in);
+        Reddit user = new User(input.toString());
+        Reddit comments = new Comments(input.toString());
+        Reddit submissions = new Submissions(input.toString());
 
         System.out.println(user);
         System.out.println(comments);
