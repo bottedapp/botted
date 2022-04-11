5f700843a50c375ff69b7a324188e0c49a5bd98b
leemoriarity <73798970+leemoriarity@users.noreply.github.com>
2022-04-11 12:03:32 -0400

    Update driver.java

--- Java_Update2/driver.java
+++ Java_Update2/driver.java
@@ -6,8 +6,7 @@ public class driver {
 
     protected ArrayList<String> keyPhrase;
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException, InterruptedException {
         ArrayList keyPhrase = new ArrayList();
         keyPhrase.add("This is a good bot!");
         keyPhrase.add("This is a bad bot!");
@@ -17,12 +16,16 @@ public class driver {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter username/post/comment: ");
         String in = scan.nextLine();
-        Reddit reddit = new Reddit(in);
-        Reddit user = new User();
+        Reddit reddit = new Reddit();
+        String redditor = reddit.readInput(in);
+        Reddit user = new User(redditor);
+        Reddit comments = new Comment(redditor);
+        Reddit submissions = new Submission(redditor);
+        Reddit bot = new Bot();
         Reddit human = new Human();
-        Reddit submissions = new Submission();
 
         System.out.println(user);
+        System.out.println(comments);
         System.out.println(submissions);
     }
 
