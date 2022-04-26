f04e5aa1e044c05184dfa8f1b1b59a0b0e072434
kalisparrow <102925817+kalisparrow@users.noreply.github.com>
2022-04-25 23:40:25 -0400

    Update BotAccount.java

--- JavaUpdate3/BotAccount.java
+++ JavaUpdate3/BotAccount.java
@@ -8,8 +8,6 @@ public class BotAccount extends UserAccount {
      * Private variables
      */
     private boolean bot;
-    private boolean goodBot;
-    private boolean badBot;
 
     /**
      * Default constructor
@@ -41,16 +39,12 @@ public class BotAccount extends UserAccount {
      * @param upvote An upvote on a post or comment
      * @param downvote A downvote on a post or comment
      * @param bot Whether this is a bot
-     * @param goodBot Whether the bot is good
-     * @param badBot Whether the bot is bad
      * @throws IOException
      * @throws InterruptedException
      */
     public BotAccount(String subreddit, String name, String id, String user, Boolean verified, Boolean has_verified_email, Boolean is_gold, Boolean is_mod, Boolean is_employee, int awardee_karma, int awarder_karma, int link_karma, int comment_karma, int total_karma, Date created, String comment, boolean upvote, boolean downvote, boolean bot, boolean goodBot, boolean badBot) throws IOException, InterruptedException {
         super(subreddit, name, id, user, verified, has_verified_email, is_gold, is_mod, is_employee, awardee_karma, awarder_karma, link_karma, comment_karma, total_karma, created, comment, upvote, downvote);
         this.bot = bot;
-        this.goodBot = goodBot;
-        this.badBot = badBot;
     }
 
     //getters
@@ -59,26 +53,26 @@ public class BotAccount extends UserAccount {
         return bot;
     }
 
-    public boolean isGoodBot() {
-        return goodBot;
-    }
-
-    public boolean isBadBot() {
-        return badBot;
-    }
-
     //setters
 
     public void setBot(boolean bot) {
         this.bot = bot;
     }
 
-    public void setGoodBot(boolean goodBot) {
-        this.goodBot = goodBot;
+    /**
+     * Return correct response according to results
+     * @param keyPhrase Phrase to signal to our bot for an action
+     * @return
+     */
+    public String responses(ArrayList<String> keyPhrase) {
+
+        String response = keyPhrase.get(0);
+
+        return response;
     }
 
-    public void setBadBot(boolean badBot) {
-        this.badBot = badBot;
+    public static Object responses() {
+        return responses();
     }
 
     /**
@@ -89,28 +83,7 @@ public class BotAccount extends UserAccount {
     public String toString() {
         return "Bot{" +
                 "bot=" + bot +
-                ", goodBot=" + goodBot +
-                ", badBot=" + badBot +
                 '}';
-    }
-
-    /**
-     * Return correct response according to results
-     * @param goodBot Whether the bot is good
-     * @param badBot Whether the bot is bad
-     * @param keyPhrase Phrase to signal to our bot for an action
-     * @return
-     */
-    public String[] responses(boolean goodBot, boolean badBot, ArrayList<String> keyPhrase) {
-        if (goodBot = true) {
-            return new String[]{keyPhrase.get(0)};
-        }
-        else if (badBot = true) {
-            return new String[]{keyPhrase.get(1)};
-        }
-        else {
-            return new String[]{keyPhrase.get(3)};
-        }
     }
 
 }
